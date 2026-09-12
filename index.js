df36e326249349d59ee827bb2516928461bc6bcc
shwkagaming6-creator <shwkagaming6@gmail.com>
2026-09-12 19:51:03 +0300

    Create package.json for project dependencies and scripts
    Added package.json configuration with dependencies and scripts.

---
+++ index.js
@@ -0,0 +1,14 @@
+{
+  "name": "bdfd-voice-backend",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.25.1",
+    "@discordjs/voice": "^0.19.0",
+    "express": "^5.1.0",
+    "dotenv": "^17.2.2"
+  }
+}
